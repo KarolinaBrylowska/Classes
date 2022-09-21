5075213832668a53a0387c93e900d5d1eb63896d
karaolinabrylowska <kar.bronk@gmail.com>
2022-09-21 19:29:55 +0200

    added class and public int floor

---
+++ src/Apartments.java
@@ -0,0 +1,6 @@
+public class Apartments {
+    public static void main(String[] args) {
+        Flat mieszkanie1 = new Flat(2, "Wrocław", true);
+        System.out.println("Mieszkanie1:" +mieszkanie1.getFloor());
+    }
+}
---
+++ src/Flat.java
@@ -0,0 +1,18 @@
+public class Flat {
+    int floor;
+    String city;
+    boolean balcony;
+    Flat(int floor, String city, boolean balcony){
+        this.balcony = balcony;
+        this.city = city;
+        this.floor = floor;
+    }
+
+    public int getFloor () {
+        return floor;
+    }
+
+    {
+
+    }
+}
